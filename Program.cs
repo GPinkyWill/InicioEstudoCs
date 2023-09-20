4d5a9c815e5da540c45f3c07fc024cd86a9b9098
GPinkyWill <guilhermemg.2000@yahoo.com>
2023-09-20 13:24:27 -0300

    upando primeiros arquivos

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoLingC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 35;
+            string age;
+            string characterName = "Jhon Jhonson";
+            string response;
+            Console.WriteLine(characterName);
+            Console.WriteLine(x);
+            Console.WriteLine(characterName + " is " + x + " years old.");
+            Console.WriteLine("Eu não...\nAcredito!\n");
+            Console.WriteLine(Math.Min(3, 60));
+            Console.WriteLine(Math.Round(5.6));
+
+            Console.WriteLine(characterName.Substring(6,6));
+            Console.WriteLine("Enter your Name:");
+            response = Console.ReadLine();
+            Console.WriteLine("Enter your Age:");
+            age = Console.ReadLine();
+            Console.WriteLine("Hello " + response + ", nice to meet you! I've heard you are "+ age +" years old!");
+;
+            Console.ReadLine();
+
+        }
+       
+    }
+
+}
